d33c30e1c49af31e088b1192126c6d6f981f2db2
shelomi123 <shelomipriskila1995@gmail.com>
2020-05-21 13:47:29 +0530

    commited

---
+++ Introduction/src/demo.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		//gh
+		//dfdffj
+		
+		//
+		
+	}
+
+}
