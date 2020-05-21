88786e0e848329c9ee58eae9c08f975939354127
shelomi123 <shelomipriskila1995@gmail.com>
2020-05-21 15:33:06 +0530

    firefox driver added

--- Introduction/src/driveChrom.java
+++ Introduction/src/driveChrom.java
@@ -1,19 +1,19 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class demo {
+public class driveChrom {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
+		
 		driver.get("https://www.google.com");
 		System.out.println(driver.getTitle());
-		//gh
-		//dfdffj
 		
-		//
+		
+		
 		
 	}
 
