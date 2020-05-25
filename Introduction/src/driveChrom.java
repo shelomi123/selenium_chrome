6d9ca1d3fc1666fc30735d70c3de8ba28945de7a
shelomi123 <shelomipriskila1995@gmail.com>
2020-05-25 16:35:37 +0530

    interviewQ added

--- Introduction/src/driveChrom.java
+++ Introduction/src/driveChrom.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,13 +10,24 @@ public class driveChrom {
 		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		
-		driver.get("https://www.google.com");
-		System.out.println(driver.getTitle());
-		System.out.println(driver.getCurrentUrl());
+		driver.get("https://facebook.com");
+		//System.out.println(driver.getTitle());
+		//System.out.println(driver.getCurrentUrl());
+		
+		driver.findElement(By.id("email")).sendKeys("Hi Shelomi");
+		driver.findElement(By.name("pass")).sendKeys("1234");
+		driver.findElement(By.linkText("Forgotten account?")).click();
+		
 		//System.out.println(driver.getPageSource());
 		
-		driver.get("http://yahoo.com");
-		driver.navigate().back();
+		//driver.navigate().to("http://yahoo.com");
+		//driver.navigate().forward();
+		//driver.navigate().back();
+		
+		//driver.close();
+		//driver.quit();
+		
+		
 	}
 
 }
---
+++ Introduction/src/facebookXpath.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class facebookXpath {
+	
+	public static void main(String[]args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("http://facebook.com/login");
+		
+		//Syntax of xpath = "//tagname[@attribute='value']"
+		//Xpath regular expression= //tagname[contains(@attribute,'value')]
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys("shelomi");
+		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("1234");
+		driver.findElement(By.xpath("//*[@name='login']")).click();
+		
+		//Synatax of cssSelector = "tagname[attribute='value']"   or   "tagname#id"   or "tagname.classname"
+		//CssSelector regular expression= tagname[attribute*='value']
+		driver.findElement(By.cssSelector("input[name='email']")).sendKeys("abcd");
+	}
+
+}
+
+
+/*
+	Relative Xpath=  Does not depend on parent nodes.. This way is good cuz it directly jumps
+	Absolute Xpath=  ex:  //section/div/div/ul/li[2]
+						  //div[@class='list-c']/div/div[2]/input
+	Identify ur sibling = //*[@id='tablist1-tab1']/following-sibling::li[1]
+	
+	Go back to parent from child = //*[@id='tablist1-tab1']/parent::ul   This is only in Xpath. Cant in cssSelector
+*/
