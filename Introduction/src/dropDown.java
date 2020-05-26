4a75407e12380787b2cee8c74f4c7dfc145c65a1
shelomi123 <shelomipriskila1995@gmail.com>
2020-05-27 03:22:05 +0530

    done

--- Introduction/src/dropDown.java
+++ Introduction/src/dropDown.java
@@ -1,11 +1,17 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class dropDown {
 	public static void main(String []args) {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://www.spicejet.com/");
+		Select s = new Select(driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")));
+		s.selectByValue("USD");
 		
 		
 	}
