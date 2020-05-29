3a9a5f97fd8c170a96c6b821f5f1bfcf788fc959
shelomi123 <shelomipriskila1995@gmail.com>
2020-05-30 03:02:48 +0530

    done

--- Introduction/src/salesForceClass.java
+++ Introduction/src/salesForceClass.java
@@ -9,7 +9,7 @@ public class salesForceClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		
 		/*driver.get("https://login.salesforce.com/?locale=eu");
