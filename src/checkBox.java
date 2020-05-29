adbd655626c493afd2ab46016704a49899e8be2c
shelomi123 <shelomipriskila1995@gmail.com>
2020-05-30 03:49:09 +0530

    commited

--- src/checkBox.java
+++ src/checkBox.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -11,6 +12,9 @@ public static void main(String []args) {
 		
 		driver.get("https://www.spicejet.com/");
 		
-		
+		WebElement a = driver.findElement(By.id("ctl00_mainContent_chk_SeniorCitizenDiscount"));
+		a.click();
+		System.out.println(a.isSelected());
+		System.out.println(driver.findElements(By.cssSelector("[type='checkbox']")).size());
 	}
 }
