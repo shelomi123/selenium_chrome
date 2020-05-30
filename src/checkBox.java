ff28f8726d1ca7d9fc57df74b05127dea865c9cd
shelomi123 <shelomipriskila1995@gmail.com>
2020-05-30 11:14:18 +0530

    assertions added

--- src/checkBox.java
+++ src/checkBox.java
@@ -1,8 +1,10 @@
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class checkBox {
 public static void main(String []args) {
@@ -13,8 +15,25 @@ public static void main(String []args) {
 		driver.get("https://www.spicejet.com/");
 		
 		WebElement a = driver.findElement(By.id("ctl00_mainContent_chk_SeniorCitizenDiscount"));
+		
+		//expect false , if the condition is false code continue
+		Assert.assertFalse(a.isSelected());
+		
 		a.click();
-		System.out.println(a.isSelected());
+		
+		//System.out.println(a.isSelected());
 		System.out.println(driver.findElements(By.cssSelector("[type='checkbox']")).size());
+		
+		//expect true , if the condition is true code continue
+		Assert.assertTrue(a.isSelected());
+		
+		driver.findElement(By.id("divpaxinfo")).click();
+		
+		Select s1 = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Adult")));
+		s1.selectByIndex(6);
+		Select s2 = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Child")));
+		s2.selectByIndex(2);
+		Select s3 = new Select(driver.findElement(By.id("ctl00_mainContent_ddl_Infant")));
+		s3.selectByIndex(1);
 	}
 }
