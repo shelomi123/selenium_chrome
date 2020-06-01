92bc805c548017674943573d29eb146b241619f2
shelomi123 <shelomipriskila1995@gmail.com>
2020-06-01 13:43:36 +0530

    cart added

--- src/assignment2.java
+++ src/assignment2.java
@@ -6,77 +6,76 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class assignment2 {
-public static void main(String[]args) {
-		
+	public static void main(String[] args) {
+
 		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
+
 		driver.get("https://www.cleartrip.com/");
-		
-		//calender
+
+		// calender
 		driver.findElement(By.id("DepartDate")).click();
 		driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-active ")).click();
-		
-		//dropbox
-		//driver.findElement(By.id("Adults")).
+
+		// dropbox
+		// driver.findElement(By.id("Adults")).
 		Select adult = new Select(driver.findElement(By.id("Adults")));
 		adult.selectByValue("3");
 		Select child = new Select(driver.findElement(By.id("Childrens")));
 		child.selectByValue("2");
 		Select infant = new Select(driver.findElement(By.id("Infants")));
 		infant.selectByValue("3");
-		
-		//link
+
+		// link
 		driver.findElement(By.id("MoreOptionsLink")).click();
-		
+
 		driver.findElement(By.id("Class")).click();
 		Select s = new Select(driver.findElement(By.id("Class")));
 		s.selectByValue("Business");
-		
+
 		driver.findElement(By.id("AirlineAutocomplete")).sendKeys("abcd");
-		
+
 		driver.findElement(By.id("SearchBtn")).click();
-		System.out.println(driver.findElement(By.id("homeErrorMessage")).getText()); 
-		
+		System.out.println(driver.findElement(By.id("homeErrorMessage")).getText());
+
 		/*
-		ANSWER
-		//calender
-
-driver.findElement(By.id("DepartDate")).click();
-
-driver.findElement(By.cssSelector(".ui-state-default.ui-state-highlight.ui-state-active ")).click();
-
-//dropbox
-
-//driver.findElement(By.id("Adults")).
-
-Select adult = new Select(driver.findElement(By.id("Adults")));
-
-adult.selectByValue("3");
-
-Select child = new Select(driver.findElement(By.id("Childrens")));
-
-child.selectByValue("2");
-
-Select infant = new Select(driver.findElement(By.id("Infants")));
-
-infant.selectByValue("3");
-g
-//link
-
-driver.findElement(By.id("MoreOptionsLink")).click();
-
-driver.findElement(By.id("Class")).click();
-
-Select s = new Select(driver.findElement(By.id("Class")));
-
-s.selectByValue("Business");
-
-driver.findElement(By.id("AirlineAutocomplete")).sendKeys("abcd");
-
-driver.findElement(By.id("SearchBtn")).click();
-
-System.out.println(driver.findElement(By.id("homeErrorMessage")).getText());
-*/
+		 * ANSWER //calender
+		 * 
+		 * driver.findElement(By.id("DepartDate")).click();
+		 * 
+		 * driver.findElement(By.
+		 * cssSelector(".ui-state-default.ui-state-highlight.ui-state-active ")).click()
+		 * ;
+		 * 
+		 * //dropbox
+		 * 
+		 * //driver.findElement(By.id("Adults")).
+		 * 
+		 * Select adult = new Select(driver.findElement(By.id("Adults")));
+		 * 
+		 * adult.selectByValue("3");
+		 * 
+		 * Select child = new Select(driver.findElement(By.id("Childrens")));
+		 * 
+		 * child.selectByValue("2");
+		 * 
+		 * Select infant = new Select(driver.findElement(By.id("Infants")));
+		 * 
+		 * infant.selectByValue("3"); g //link
+		 * 
+		 * driver.findElement(By.id("MoreOptionsLink")).click();
+		 * 
+		 * driver.findElement(By.id("Class")).click();
+		 * 
+		 * Select s = new Select(driver.findElement(By.id("Class")));
+		 * 
+		 * s.selectByValue("Business");
+		 * 
+		 * driver.findElement(By.id("AirlineAutocomplete")).sendKeys("abcd");
+		 * 
+		 * driver.findElement(By.id("SearchBtn")).click();
+		 * 
+		 * System.out.println(driver.findElement(By.id("homeErrorMessage")).getText());
+		 */
 	}
 }
---
+++ src/cartArray.java
@@ -0,0 +1,41 @@
+//import java.awt.List;
+import java.util.Arrays;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class cartArray {
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		String vegesNeeded[]= {"Cucumber","Beetroot"};
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+
+		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
+		
+		for(int i=0; i<products.size();i++) {
+			
+			
+			String[] name= products.get(i).getText().split("-");
+			String formattedName = name[0].trim();
+			
+			//should convert array into List cuz contains support for List
+			//Initially  we did not implement arraylist to save memory
+			List vegeList = Arrays.asList(vegesNeeded);
+			
+			int j=0;
+			if(vegeList.contains(formattedName)) {
+				j++;
+				driver.findElements(By.xpath("//button[@type='button']")).get(i).click();
+				if(j==2)
+					break;
+			}
+		}
+
+	}
+}
