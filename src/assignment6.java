017e5f31735fdf2d5e80ef11362c0d8c9dbfb336
shelomi123 <shelomipriskila1995@gmail.com>
2020-06-18 14:27:26 +0530

    done

--- src/assignment6.java
+++ src/assignment6.java
@@ -44,3 +44,46 @@ public class assignment6 {
 		
 	}
 }
+
+
+
+/*	ANSWER
+ * driver.get("http://qaclickacademy.com/practice.php");
+
+        driver.findElement(By.xpath("//*[@id='checkbox-example']/fieldset/label[2]/input")).click();
+
+        String opt=driver.findElement(By.xpath("//*[@id='checkbox-example']/fieldset/label[2]")).getText();
+
+        WebElement dropdown=driver.findElement(By.id("dropdown-class-example"));
+
+        Select s=new Select(dropdown);
+
+        s.selectByVisibleText(opt);
+
+        driver.findElement(By.name("enter-name")).sendKeys(opt);
+
+        driver.findElement(By.id("alertbtn")).click();
+
+     String text=  driver.switchTo().alert().getText();
+
+     if(text.contains(opt))
+
+     {
+
+    System.out.println("Alert message success");
+
+     }
+
+     else
+
+    System.out.println("Something wrong with execution");
+
+}
+
+       
+
+       
+
+     //  System.out.println( driver.findElement(By.xpath("//*[@id='checkbox-example']/fieldset/label[2]")).getText());   
+
+       */
