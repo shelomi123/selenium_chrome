03ac7fe26f9135a53b05bcf82f257cb99dc66453
shelomi123 <shelomipriskila1995@gmail.com>
2020-06-19 14:35:14 +0530

    table modified

--- src/table.java
+++ src/table.java
@@ -13,15 +13,26 @@ public class table {
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://www.cricbuzz.com/live-cricket-scorecard/18970/pak-vs-sl-2nd-t20i-pakistan-v-sri-lanka-in-uae-2017");
+		//creat a table element
 		WebElement table=driver.findElement(By.cssSelector("div[class='cb-col cb-col-100 cb-ltst-wgt-hdr']"));
 		int rowcount= table.findElements(By.cssSelector("cb-col cb-col-100 cb-scrd-itms")).size();
-		//int count=table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).size();
 		int count=table.findElements(By.cssSelector("div[class='cb-col cb-col-100 cb-scrd-itms'] div:nth-child(3)")).size();
 		
-
-		for (int i = 0; i < count; i++) {
-			System.out.println(table.findElements(By.xpath("//div[@class='cb-col cb-col-100 cb-scrd-itms']/div[3]")).get(i).getText());
+		int sum=0;
+		for (int i = 0; i < count-2; i++) {
+			//System.out.println(table.findElements(By.xpath("//div[@class='cb-col cb-col-100 cb-scrd-itms']/div[3]")).get(i).getText());
+			String value = table.findElements(By.xpath("//div[@class='cb-col cb-col-100 cb-scrd-itms']/div[3]")).get(i).getText();
+			int valueInt = Integer.parseInt(value);
+			sum= sum + valueInt ;
+			
 		}
-
+		
+		String extra = driver.findElement(By.xpath("//div[text()='Extras']/following-sibling::div")).getText();
+		int extraInt = Integer.parseInt(extra);
+		
+		System.out.println( sum + extraInt );
+		
+		System.out.println(driver.findElement(By.xpath("//div[text()='Total']/following-sibling::div")).getText());
+		
 	}
 }
