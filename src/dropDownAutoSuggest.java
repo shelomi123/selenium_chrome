c9a35318fa2956533df3cd353aa888d78d222d0f
shelomi123 <shelomipriskila1995@gmail.com>
2020-06-19 22:51:00 +0530

    done

--- src/dropDownAutoSuggest.java
+++ src/dropDownAutoSuggest.java
@@ -6,22 +6,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class dropDownAutoSuggest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://ksrtc.in/oprs-web/guest/home.do?h=1");
-		driver.findElement(By.id("fromPlaceName")).sendKeys("BENG");
+		driver.findElement(By.id("fromPlaceName")).sendKeys("BEN");
+		
+		Thread.sleep(4000L);
+		
 		driver.findElement(By.id("fromPlaceName")).sendKeys(Keys.DOWN);
 		driver.findElement(By.id("fromPlaceName")).sendKeys(Keys.DOWN);
 		
 		//selenium can not get hidden elements but javascript can
-		
+		//Javascript DOM can extract hidden elements
+		//because selenium cannot identify hidden elements - (Ajax implementation)
+		//investigate the properties of object if it have any hidden text
+
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 		
 		String script = "return document.getElementById(\"fromPlaceName\").value;";
 		String text = (String) js.executeScript(script);
 		System.out.println(text);
+		
+		while(!text.equalsIgnoreCase("BENGALUgRU AIRPORT")) {
+			driver.findElement(By.id("fromPlaceName")).sendKeys(Keys.DOWN);
+			
+			text = (String) js.executeScript(script);
+			System.out.println(text);
+		}
 	}
 }
