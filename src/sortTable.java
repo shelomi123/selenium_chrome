3af8c2b99cdcb411277eaa83274f9b79d2dbb8b4
shelomi123 <shelomipriskila1995@gmail.com>
2020-06-20 22:32:26 +0530

    done

--- src/sortTable.java
+++ src/sortTable.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class sortTable {
 	public static void main(String[] args) {
@@ -16,8 +17,10 @@ public class sortTable {
 
 		driver.get("https://www.rahulshettyacademy.com/seleniumPractise/#/offers");
 		
+		//Initially not sorted. Click once sorted descending. Click again sorted ascending
+		driver.findElement(By.xpath("//thead/tr/th[2]")).click();
 		//driver.findElement(By.xpath("//thead/tr/th[2]")).click();
-		//driver.findElement(By.xpath("//thead/tr/th[2]")).click();
+		
 		
 		List<WebElement> items = driver.findElements(By.xpath("//tbody/tr/td[2]"));
 		// CssSelector = tr td:nth-child(2)
@@ -33,8 +36,10 @@ public class sortTable {
 		for(int j=0;j<original.size();j++) {
 			copied.add(original.get(j));
 		}
-		
+		//////////////////////////////////////////////////////ASCENDING
 		Collections.sort(copied);
+		///////////////////////////////////////////////////////DESCENDING
+		Collections.reverse(copied);
 		
 		////////////////////////////////////////////////////////////////////
 		for(String s:copied) {
@@ -47,6 +52,7 @@ public class sortTable {
 		}
 		
 		//////////////////////////////////////////////////////////////////////////
+		Assert.assertTrue(copied.equals(original));
 		if(copied.equals(original)) {
 			System.out.println("Initially sorted");
 		}
